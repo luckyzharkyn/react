bcc4ed0d604de11ca1c29e8c06121ea44e88658f
Zharkyn <Lucky_zharkyn@mail.ru>
2023-12-17 18:35:06 +0600

    1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import uuid from 'react-uuid';
 
 function App() {
-  return <div class="eee">
-    <b>waw baby</b>
-  </div>;
+  function id() {
+    return uuid();
+  }
+
+  const [count, setCount] = useState(0);
+
+  return <div>
+    <span>{count}</span>
+    <button onClick={() => setCount(count + 1)}>+</button>
+  </div>
 }
 
 export default App;
+
+
+
+// Я пошел в магазин рядом с домом 17.12.2023
+// 18:33 вышел из дома
+// написал на всякий случай
