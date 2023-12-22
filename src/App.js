76405211a8905d22bec0c60159faf65b5c29ef9c
Zharkyn Markabayev <Lucky_zharkyn@mail.ru>
2023-12-22 13:07:39 +0600

    Реактивные операции над массивами объектов в React       почти закончил

--- src/App.js
+++ src/App.js
@@ -2,38 +2,147 @@ import React, { useState } from 'react';
 import uuid from 'react-uuid';
 
 function App() {
-  const[notes, setNotes] = useState([1, 2, 3, 4, 5]);
-  const[value, setValue] = useState();
-  const[index, setIndex] = useState(null);
-  
+
+  const initNotes = [
+    {
+      id: id(),
+      prop1: 'value11',
+      prop2: 'value12',
+      prop3: 'value13',
+    },
+    {
+      id: id(),
+      prop1: 'value21',
+      prop2: 'value22',
+      prop3: 'value23',
+    },
+    {
+      id: id(),
+      prop1: 'value31',
+      prop2: 'value32',
+      prop3: 'value33',
+    },
+  ];
+
+  const inputsValue = [
+    {
+      id: id(),
+      value: ""
+    },
+    {
+      id: id(),
+      value: ""
+    },
+    {
+      id: id(),
+      value: ""
+    },
+  ]
+
+  const [notes, setNotes] = useState(initNotes);
+  const [value, setValue] = useState(inputsValue);
 
   const result = notes.map((elem, index) => {
     return <>
-      <li key={index} onClick={() => doSmth(index)}>{elem}</li>
+      <li key={elem.id}>
+        <span> {elem.prop1} </span>
+        <span> {elem.prop2} </span>
+        <span> {elem.prop3} </span>
+      </li>
+      <button onClick={() => addValueToInput(elem)}>add value to input</button>
+      <button onClick={() => deleteThis(elem.id)}>delete</button>
     </>
-  })
+  });
+  const inputs = value.map((inputObj, index) => {
+    return <>
+      <input type='text' key={inputObj.id} onChange={(event) => updateValue(event, inputObj.id)} value={inputObj.value}/>
+      <button key={inputObj.id} onClick={setValueToInput(inputObj.id, index)}>change</button>
+    </>
+  });
 
-  function doSmth(index) {
-    setIndex(index);
-    setValue(notes[index]);
+  function updateValue(event, id) {
+    let copy = Object.assign([], value);
+    for(let obj of copy) {
+      if(obj.id == id) {
+        obj.value = event.target.value;
+      }
+    }
+    setValue(copy);
   }
 
-  function changeInput(event) {
-    // let copy = [...notes.slice(0, index), event.target.value, ...notes.slice(index + 1)];
+  function setValueToInput(inputID, inputIndex) {
+    let text = "";
+    inputs.forEach(elem => {
+      if(inputID == elem.id) {
+        text = elem.value;
+      }
+    })
+
+    // TODO
+  }
+
+  function deleteThis(id) {
     let copy = Object.assign([], notes);
-    copy[index] = event.target.value;
+    setNotes(copy.filter(elem => {
+      if(elem.id !== id) {
+        return elem;
+      }
+    }))
+  }
+
+  function addValueToInput(elem) {
+    let input1 = elem.prop1,
+        input2 = elem.prop2,
+        input3 = elem.prop3;
+
+    let copy = Object.assign([], value);
+    copy = copy.map((obj, index) => {
+      if(index == 0) {
+        obj.value = input1;
+      }
+      if(index == 1) {
+        obj.value = input2;
+      }
+      if(index == 2) {
+        obj.value = input3;
+      }
+      return obj;
+    })
+    setValue(copy)
+  }
+
+  function addNewElement() {
+    let copy = Object.assign([], notes);
+    let inputValue1 = "";
+    let inputValue2 = "";
+    let inputValue3 = "";
+    value.forEach((obj, index) => {
+      if(index == 0) {
+        inputValue1 = obj.value;
+      }
+      if(index == 1) {
+        inputValue2 = obj.value;
+      }
+      if(index == 2) {
+        inputValue3 = obj.value;
+      }
+    })
+    copy.push({
+      id: id(),
+      prop1: inputValue1,
+      prop2: inputValue2,
+      prop3: inputValue3,
+    })
     setNotes(copy);
   }
-  function reverse() {
-    let copy = Object.assign([], notes);
-    setNotes(copy.reverse());
-  }
+
+
 
   return <div>
-    <button onClick={() => reverse() }>reverse</button>
-    <input value={value} onBlur={(event) => changeInput(event)} onChange={(event) => setValue(event.target.value)}/>
     <ul>
-    {result}
+      {inputs}
+      {/* <button onClick={() => addNewElement()}>add new li</button> */}
+      { result  }    
     </ul>
   </div>
 }
