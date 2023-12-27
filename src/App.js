f3df153e6fa3fcc060655b74e9bf614bf232ae8f
Zharkyn Markabayev <Lucky_zharkyn@mail.ru>
2023-12-27 14:30:40 +0600

    дошел до введение в компоненты

--- src/App.js
+++ src/App.js
@@ -6,144 +6,49 @@ function App() {
   const initNotes = [
     {
       id: id(),
-      prop1: 'value11',
-      prop2: 'value12',
-      prop3: 'value13',
+      name: 'name1',
+      desc: 'long description 1',
+      show: false,
     },
     {
       id: id(),
-      prop1: 'value21',
-      prop2: 'value22',
-      prop3: 'value23',
+      name: 'name2',
+      desc: 'long description 2',
+      show: false,
     },
     {
       id: id(),
-      prop1: 'value31',
-      prop2: 'value32',
-      prop3: 'value33',
+      name: 'name3',
+      desc: 'long description 3',
+      show: false,
     },
   ];
 
-  const inputsValue = [
-    {
-      id: id(),
-      value: ""
-    },
-    {
-      id: id(),
-      value: ""
-    },
-    {
-      id: id(),
-      value: ""
-    },
-  ]
-
   const [notes, setNotes] = useState(initNotes);
-  const [value, setValue] = useState(inputsValue);
 
-  const result = notes.map((elem, index) => {
-    return <>
-      <li key={elem.id}>
-        <span> {elem.prop1} </span>
-        <span> {elem.prop2} </span>
-        <span> {elem.prop3} </span>
-      </li>
-      <button onClick={() => addValueToInput(elem)}>add value to input</button>
-      <button onClick={() => deleteThis(elem.id)}>delete</button>
+  const result = notes.map(note => {
+		return <>
+    <p key={note.id}>
+			{note.name}, 
+			{note.show && <i>{note.desc}</i>}
+		</p>
+    <button key={note.id} onClick={() => showElement(note.id)}>{note.show ? "hide" : "show"}</button>
     </>
-  });
-  const inputs = value.map((inputObj, index) => {
-    return <>
-      <input type='text' key={inputObj.id} onChange={(event) => updateValue(event, inputObj.id)} value={inputObj.value}/>
-      <button key={inputObj.id} onClick={setValueToInput(inputObj.id, index)}>change</button>
-    </>
-  });
+	});
 
-  function updateValue(event, id) {
-    let copy = Object.assign([], value);
-    for(let obj of copy) {
-      if(obj.id == id) {
-        obj.value = event.target.value;
-      }
-    }
-    setValue(copy);
-  }
-
-  function setValueToInput(inputID, inputIndex) {
-    let text = "";
-    inputs.forEach(elem => {
-      if(inputID == elem.id) {
-        text = elem.value;
-      }
-    })
-
-    // TODO
-  }
-
-  function deleteThis(id) {
+  function showElement(id) {
     let copy = Object.assign([], notes);
-    setNotes(copy.filter(elem => {
-      if(elem.id !== id) {
-        return elem;
+    copy = copy.map(elem => {
+      if(elem.id === id) {
+        elem.show = !elem.show;
       }
-    }))
-  }
-
-  function addValueToInput(elem) {
-    let input1 = elem.prop1,
-        input2 = elem.prop2,
-        input3 = elem.prop3;
-
-    let copy = Object.assign([], value);
-    copy = copy.map((obj, index) => {
-      if(index == 0) {
-        obj.value = input1;
-      }
-      if(index == 1) {
-        obj.value = input2;
-      }
-      if(index == 2) {
-        obj.value = input3;
-      }
-      return obj;
-    })
-    setValue(copy)
-  }
-
-  function addNewElement() {
-    let copy = Object.assign([], notes);
-    let inputValue1 = "";
-    let inputValue2 = "";
-    let inputValue3 = "";
-    value.forEach((obj, index) => {
-      if(index == 0) {
-        inputValue1 = obj.value;
-      }
-      if(index == 1) {
-        inputValue2 = obj.value;
-      }
-      if(index == 2) {
-        inputValue3 = obj.value;
-      }
-    })
-    copy.push({
-      id: id(),
-      prop1: inputValue1,
-      prop2: inputValue2,
-      prop3: inputValue3,
-    })
+      return elem;
+    });
     setNotes(copy);
   }
 
-
-
   return <div>
-    <ul>
-      {inputs}
-      {/* <button onClick={() => addNewElement()}>add new li</button> */}
-      { result  }    
-    </ul>
+    {result}
   </div>
 }
 
